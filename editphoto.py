1bc4129a3f774b65612dc925659c50118a1159cd
Haardispro <62053026+Haardispro@users.noreply.github.com>
2021-03-25 20:45:27 +0530

    second commit

--- editphoto.py
+++ editphoto.py
@@ -15,14 +15,17 @@ root.geometry("800x600")
 root.configure(bg="white")
 
 #Open and save
-open_file = Button(root, text="Open file", command=openfile)
+open_file = Button(root, text="Open file", command=openfile) 
 open_file.grid(row=0, column=0)
 save_file = Button(root, text="Save File")
 
 #Editing functions
 #---changing the size of the image using thumbnail function
 
+size = Button(root, text="Select Image size")
+#Change dominant color
 
+dom_col = Button(root, text="Change the dominant color of the image")
 
 
 root.mainloop()
