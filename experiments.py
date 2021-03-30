186ddf828727af36fdd06bd576f283319764f5fb
Haardispro <62053026+Haardispro@users.noreply.github.com>
2021-03-30 21:30:20 +0530

    Class stuff

--- experiments.py
+++ experiments.py
@@ -4,30 +4,35 @@ from tkinter import filedialog
 import os
 
 
+
+
+class MainWindow:
+    def __init__(self, master):
+        self.master = master
+        master.title("Photo Editor")
+        master.geometry("550x300+300+150")
+        master.resizable(width=True, height=True)
+
+        self.btn = Button(master, text='open image', command=self.open_img)
+        self.btn.pack()
+        self.btn_blur = Button(master, text="blur image", command=self.blur_btn)
+        self.btn_blur.pack()
+
+    def openfn(self):
+        filename = filedialog.askopenfilename(title='open')
+        return filename
+    def open_img(self):
+        x = self.openfn
+        img = Image.open(x)
+        #img = img.resize((600, 600), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        panel = Label(root, image=img)
+        panel.image = img
+        panel.pack()
+    def blur_btn(self):
+
+        return
+
 root = Tk()
-root.geometry("550x300+300+150")
-root.resizable(width=True, height=True)
-root.title("Photo Editor")
-
-def openfn():
-    filename = filedialog.askopenfilename(title='open')
-    return filename
-def open_img():
-    x = openfn()
-    img = Image.open(x)
-    #img = img.resize((600, 600), Image.ANTIALIAS)
-    img = ImageTk.PhotoImage(img)
-    panel = Label(root, image=img)
-    panel.image = img
-    panel.pack()
-def blur_btn():
-
-    return
-
-
-btn = Button(root, text='open image', command=open_img)
-btn.pack()
-btn_blur = Button(root, text="blur image", command=blur_btn)
-btn_blur.pack()
-
+my_window = MainWindow(root)
 root.mainloop()
